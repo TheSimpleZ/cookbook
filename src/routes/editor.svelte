2ba24eea3faac090e066ca41ce018a1caa0185bf
Zrean Tofiq <mote93@gmail.com>
2020-12-13 18:52:16 +0000

    another

--- src/routes/editor.svelte
+++ src/routes/editor.svelte
@@ -1,19 +1,19 @@
 <script context="module">
   export async function preload(page, session) {
-    let { user } = session;
+    let { user } = session
     if (!user) {
-      return this.redirect(302, "/");
+      return this.redirect(302, '/')
     }
   }
 </script>
 
 <script>
-  import { onMount } from "svelte";
+  import { onMount } from 'svelte'
   
   onMount(async () => {
-    const EditorJS = await import("@editorjs/editorjs");
-    const editor = new EditorJS.default();
-  });
+    const EditorJS = await import('@editorjs/editorjs')
+    new EditorJS.default()
+  })
 </script>
 
 <div id="editorjs" />
