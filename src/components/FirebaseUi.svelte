fb4c0efe01524fc324a216f4bbce510435fb7e79
Zrean Tofiq <mote93@gmail.com>
2020-12-14 00:56:05 +0000

    refactor firebaseui

--- src/components/FirebaseUi.svelte
+++ src/components/FirebaseUi.svelte
@@ -3,12 +3,10 @@
   import 'firebaseui/dist/firebaseui.css'
   import { stores } from '@sapper/app'
   import { onMount } from 'svelte'
-  import { Card, CardText } from 'svelte-materialify/src'
   import Cookies from 'js-cookie'
 
-
+  export let onLoad
   const { session } = stores()
-  let loading = true
 
   onMount(async () => {
     const fbui = await import('firebaseui')
@@ -21,11 +19,7 @@
         // Return type determines whether we continue the redirect automatically
         // or whether we leave that to developer to handle.
         signInSuccessWithAuthResult: () => true,
-        uiShown: () => {
-          // The widget is rendered.
-          // Hide the loader.
-          loading = false
-        },
+        uiShown: onLoad,
       },
       // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
       signInFlow: 'popup',
@@ -60,18 +54,5 @@
   })
 </script>
 
-<div class="w-full flex justify-center items-center">
-  <div class="flex-initial">
-    <Card style="max-width:300px;" {loading} >
-      <div class="pl-4 pr-4 pt-3">
-        <span class="text-overline">Welcome to the Cookbook app</span>
-        <br />
-        <span class="text-h5 mb-2">Login</span>
-        <br />
-      </div>
-      <CardText>
-        <div id="firebaseui-auth-container" />
-      </CardText>
-    </Card>
-  </div>
-</div>
+
+<div id="firebaseui-auth-container" />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,10 +8,29 @@
 
 <script>
   import FirebaseUi from '../components/FirebaseUi.svelte'
+  import { Card, CardText } from 'svelte-materialify/src'
+
+  let loading = true
+
 </script>
 
 <svelte:head>
   <title>Cookbook Login</title>
 </svelte:head>
 
-<FirebaseUi />
+
+<div class="w-full flex justify-center items-center">
+  <div class="flex-initial">
+    <Card style="max-width:300px;" {loading} >
+      <div class="pl-4 pr-4 pt-3">
+        <span class="text-overline">Welcome to the Cookbook app</span>
+        <br />
+        <span class="text-h5 mb-2">Login</span>
+        <br />
+      </div>
+      <CardText>
+        <FirebaseUi onLoad={() => {loading = false}} />
+      </CardText>
+    </Card>
+  </div>
+</div>
