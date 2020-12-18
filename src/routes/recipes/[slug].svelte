d2e2aa1ef47ed0047fcfcd8f099c0608d6e98842
Zrean Tofiq <mote93@gmail.com>
2020-12-18 20:53:53 +0100

    fix

--- src/routes/recipes/[slug].svelte
+++ src/routes/recipes/[slug].svelte
@@ -32,7 +32,9 @@
   // Rerender recipe when page changes
   $: {
     if(editor && $page.params.slug != $currentRecipe.id) {
-      if(($currentRecipe.instructions.blocks||[]).length > 0)
+      if($currentRecipe.instructions 
+        && $currentRecipe.instructions.blocks 
+        && $currentRecipe.instructions.blocks.length > 0)
       {
         autosave = false
         editor.blocks.render($currentRecipe.instructions).then(() => {autosave = true})
@@ -64,7 +66,7 @@
 
 
   <div class="flex flex-column flex-1">
-    <h3 contenteditable bind:textContent={$currentRecipe.name} class="m-auto p-5 outline-none focus:underline" style="width: fit-content"></h3>
+    <h3>{$currentRecipe.name}</h3>
     <EditorJs class="flex-1" bind:editor config={editorConfig}/>
   </div>
 
